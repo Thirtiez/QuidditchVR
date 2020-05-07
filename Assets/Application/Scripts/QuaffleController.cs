5ac8ca0c91974b440d9d76caeef91ae07a8ccb1f
ThirtiesReply <d.trenti@reply.it>
2020-05-07 09:07:19 +0200

    Added quaffle / Added pitch

---
+++ Assets/Application/Scripts/QuaffleController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using VRTK.GrabAttachMechanics;
+
+public class QuaffleController : MonoBehaviour
+{
+    [SerializeField] private VRTK_InteractableObject interactableObject;
+    [SerializeField] private TrailRenderer trailRenderer;
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private float spawnForce = 10.0f;
+
+    private Rigidbody playerRigidbody;
+
+    public void Awake()
+    {
+        interactableObject = interactableObject ?? GetComponent<VRTK_InteractableObject>();
+        rigidbody = rigidbody ?? GetComponent<Rigidbody>();
+        trailRenderer = trailRenderer ?? GetComponent<TrailRenderer>();
+
+        interactableObject.InteractableObjectGrabbed += InteractableObjectGrabbed;
+        interactableObject.InteractableObjectUngrabbed += InteractableObjectUngrabbed;
+    }
+
+    public void Start()
+    {
+        SpawnThrow();
+    }
+
+    public void SetPlayerRigidbody(Rigidbody playerRigidbody)
+    {
+        this.playerRigidbody = playerRigidbody;
+    }
+
+    private void SpawnThrow()
+    {
+        rigidbody.AddRelativeForce(Vector3.up * spawnForce);
+        InheritVelocity();
+    }
+
+    private void InheritVelocity()
+    {
+        rigidbody.velocity += playerRigidbody.velocity;
+    }
+
+    private void InteractableObjectGrabbed(object sender, InteractableObjectEventArgs e)
+    {
+        trailRenderer.enabled = false;
+    }
+
+    private void InteractableObjectUngrabbed(object sender, InteractableObjectEventArgs e)
+    {
+        trailRenderer.enabled = true;
+        InheritVelocity();
+    }
+}
---
+++ Assets/Application/Scripts/QuaffleSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class QuaffleSpawn : MonoBehaviour
+{
+    [SerializeField] private VRTK_ControllerEvents controllerEvents;
+    [SerializeField] private QuaffleController Quaffle;
+    [SerializeField] private Rigidbody playerRigidbody;
+
+    private QuaffleController currentQuaffle;
+
+    public void Awake()
+    {
+        controllerEvents = controllerEvents ?? VRTK_DeviceFinder.GetControllerRightHand().GetComponentInChildren<VRTK_ControllerEvents>();
+        controllerEvents.ButtonTwoPressed += ButtonTwoPressed;
+    }
+
+    private void ButtonTwoPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        DestroyQuaffle();
+        SpawnQuaffle();
+    }
+    public void DestroyQuaffle()
+    {
+        if (currentQuaffle != null)
+        {
+            Destroy(currentQuaffle.gameObject);
+        }
+    }
+
+    public void SpawnQuaffle()
+    {
+        currentQuaffle = Instantiate(Quaffle, transform);
+        currentQuaffle.SetPlayerRigidbody(playerRigidbody);
+    }
+}
